71728098eb94d88821c3b28d94fb837f3006119c
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2025-02-15 17:29:31 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: canguyen <canguyen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/28 14:34:01 by canguyen          #+#    #+#             */
+/*   Updated: 2025/02/03 15:34:21 by canguyen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <stddef.h> // size_t
+//# include <sys/types.h>
+//# include <stddef.h> // size_t
 # include <stdlib.h> //malloc, free
 # include <unistd.h> //read
 
@@ -9,12 +20,19 @@
 #  define BUFFER_SIZE 42
 # endif
 
+# if BUFFER_SIZE > 8000000
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 8000000
+# endif
+
 char	*get_next_line(int fd);
-int	has_newline(char *str);
+int		has_newline(char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
-int	ft_strlen(char *s);
-char *get_before_newline(const char *s);
+int		ft_strlen(char *s);
+char	*get_before_newline(const char *s);
 char	*get_after_newline(const char *s);
 char	*ft_strdup(char *src);
+void	ft_read(int fd, char **rest, char **tmp);
+void	ft_free(char **str, char **str2, char **str3);
 
 #endif
