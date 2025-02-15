5b8c085bea8e52d68daf42b36c77a8d52391581e
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2025-02-15 17:28:21 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,114 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: canguyen <canguyen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/28 14:34:58 by canguyen          #+#    #+#             */
+/*   Updated: 2025/02/03 15:36:54 by canguyen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
-	char	*buf;
-	int		r;
-	static char    *rest = NULL;
-	char *line;        // = NULL
-	char *tmp;
-	
-	r = 1;
+	static char	*rest = NULL;
+	char		*tmp;
+	char		*line;
+
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	line = NULL;
+	tmp = NULL;
+	ft_read(fd, &rest, &tmp);
+	if (rest != NULL && *rest != '\0')
+	{
+		tmp = ft_strdup(rest);
+		ft_free(&rest, 0, 0);
+		line = get_before_newline(tmp);
+		rest = get_after_newline(tmp);
+		ft_free(&tmp, 0, 0);
+	}
+	if (!line || *line == '\0')
+	{
+		ft_free(&line, &rest, &tmp);
+		return (NULL);
+	}
+	return (line);
+}
+
+void	ft_read(int fd, char **rest, char **tmp)
+{
+	char	*buf;
+	int		r;
+
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
-		return (NULL);
+		return ;
+	r = 1;
 	while (r > 0)
 	{
 		r = read(fd, buf, BUFFER_SIZE);
 		if (r == -1)
 		{
-			// free
-			return (NULL);
+			ft_free(&buf, rest, 0);
+			return ;
 		}
 		buf[r] = '\0';
-		tmp = rest;
-		rest = ft_strjoin(rest, buf);
-		free(tmp);
+		if (*rest == NULL)
+			*rest = ft_strdup("");
+		*tmp = ft_strdup(*rest);
+		ft_free(rest, 0, 0);
+		*rest = ft_strjoin(*tmp, buf);
+		ft_free(tmp, 0, 0);
 		if (has_newline(buf))
-			break ; // espace ?
+			break ;
 	}
-	line = get_before_newline(rest);
-	tmp = rest;
-	rest = get_after_newline(tmp);
-	free(tmp);
-	return (line);
+	ft_free(&buf, 0, 0);
 }
 
-int	has_newline(char *str)
+char	*get_before_newline(const char *s)
 {
-	int	i;
-
-	// const char
-	if (!str)
-		return 0;
-	i = 0;
-	while (str[i])
-	{
-		if (str[i++] == '\n')
-			return (1);
-	}
-	return (0);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char *s;
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	if (!s1){
-		return ft_strdup((char *)s2); // whyyy
-	}
-	s = malloc(sizeof(char) * (ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1));
-	if (s == NULL)
-		return NULL;
-	while (s1[i]){
-		s[i] = s1[i];
-		i++;
-	}
-	while (s2[j]){
-		s[i + j] = s2[j];
-		j++;
-	}
-	s[i + j] = '\0';
-	return s;
-}
-
-int	ft_strlen(char *s){
-	int len;
-	int i;
-
-	len = 0;
-	i = 0;
-	while (s[i++]){
-		len++;
-	}
-	return len;
-}
-
-char *get_before_newline(const char *s){
-	char *rest;
-	int i;
+	char	*rest;
+	int		i;
 
 	i = 0;
 	while (s[i] && s[i] != '\n')
 		i++;
-	if (s[i] && s[i] == '\n'){
+	if (s[i] && s[i] == '\n')
 		i++;
-	}
-	rest = malloc(sizeof(rest) * (i+1)); // ft_malloc_zero(i + 1, sizeof(rest))
+	rest = malloc(sizeof(char) * (i + 1));
 	if (!rest)
-		return NULL;
+		return (NULL);
 	i = 0;
-	while (s[i] && s[i] != '\n'){
+	while (s[i] && s[i] != '\n')
+	{
 		rest[i] = s[i];
 		i++;
 	}
-	if (s[i] == '\n'){
+	if (s[i] == '\n')
+	{
 		rest[i] = s[i];
 		i++;
 	}
@@ -116,13 +97,15 @@ char *get_before_newline(const char *s){
 	return (rest);
 }
 
-char	*get_after_newline(const char *s){
-	char *rest;
-	int i;
-	int j;
+char	*get_after_newline(const char *s)
+{
+	char	*rest;
+	int		i;
+	int		j;
 
 	j = 0;
-	while (s[j]){
+	while (s[j])
+	{
 		j++;
 	}
 	i = 0;
@@ -130,31 +113,15 @@ char	*get_after_newline(const char *s){
 		i++;
 	if (s[i] && s[i] == '\n')
 		i++;
-	rest = malloc(sizeof(rest) * (j - i  + 1)); // ft_malloc_zero
+	rest = malloc(sizeof(char) * (j - i + 1));
 	if (!rest)
-		return NULL;
+		return (NULL);
 	j = 0;
-	while (s[i + j]){
+	while (s[i + j])
+	{
 		rest[j] = s[i + j];
 		j++;
 	}
+	rest[j] = '\0';
 	return (rest);
-}
-
-char	*ft_strdup(char *src)
-{
-	char	*dup;
-	int		j;
-
-	j = 0;
-	dup = malloc(sizeof * dup * (ft_strlen(src) + 1));
-	if (dup == NULL)
-		return (NULL);
-	while (src[j] != '\0')
-	{
-		dup[j] = src[j];
-		j++;
-	}
-	dup[j] = '\0';
-	return (dup);
 }
