1173a0f2693937d2cfe011096784ef0ba49ac026
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2025-02-15 17:29:14 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,43 +7,24 @@ int	main(void)
 	char *line;
 	int fd;
 
-	fd = open("file.txt", O_RDONLY);
+	fd = open("get_next_line.h", O_RDONLY);
 	if (fd == -1)
 	{
 		perror("Erreur d'ouverture");
 		return (1);
 	}
-
-	/*while ((line = get_next_line(fd)))
-	{
+	while ((line = get_next_line(fd))){
 		printf("%s", line);
 		free(line);
-	}*/
-line = get_next_line(fd);
-printf("%s", line);
-free(line);
-
-	/*char buffer[BUF_SIZE];
-	int bytesRead;
-	int fd;
-
-	fd = open("file.txt", O_RDONLY);
-	if (fd == -1)
-	{
-		perror("Erreur lors de l'ouverture du fichier");
-		return (1);
 	}
-	// get_next_line(fd);
-	bytesRead = read(fd, buffer, BUF_SIZE);
-	if (bytesRead == -1)
-	{
-		perror("Erreur lors de la lecture du fichier");
-		close(fd);
-		return (1);
-	}
-
-	buffer[bytesRead] = '\0';
-	printf("%s", buffer);*/
 	close(fd);
 	return (0);
+
+	/*char *line;
+
+	while ((line = get_next_line(0))){
+		printf("%s", line);
+		free(line);
+	}
+	return (0);*/
 }
